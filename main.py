c18d0e2f6d0123ec15e13d8a72b021f339671ce4
daria-yermakova <daria.yerm0710@gmail.com>
2023-04-23 13:29:15 +0200

    testdata+preprocessing

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+# import wget
+# url = 'https://storage.googleapis.com/kaggle-data-sets/38237/502524/bundle/archive.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20230419%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20230419T153022Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=7ad631fe9ee3300484382ce5696ceda9c17b3e017477e4fa2423313939265836d71ed83354b47fe67cbc585bac8ebea623aa767cae8d5e0a75d5b1ab0aec5fee854e3c2ed2adbca774f40ef5b76f035e90def63669f2c0e45843699509391f45c42c3d2c1e2c3feda6a6c9457d54c775370af27dc8fb83898a163e87e15bc3983bfaf77239b1a724d02a4829824d56a466299509c711041a4e38b8bf04d1dd26a1b9aff7efa0b7c9462862414c949b4192a9ee36972ba6c88f64990d2667776f09e52a78f194b38bd6f746b24bac709e39e579158da8a1c09dab338a06f6c78a389d2d3e7aa2e20e892f6e2b803d2830be66f4fc320f9399ae2609d36952f8a4'
+# wget.download(url, 'data_path/train.zip')
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
